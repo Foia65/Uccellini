588c53a906847928b3d79780c68a1dbddf3dc886
Francesco Foianesi <foiasoft@gmail.com>
2026-02-26 15:41:08 +0100

    Initial Commit

---
+++ Uccellini/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Uccellini
+//
+//  Created by Francesco Foianesi on 26/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Uccellini/UccelliniApp.swift
@@ -0,0 +1,17 @@
+//
+//  UccelliniApp.swift
+//  Uccellini
+//
+//  Created by Francesco Foianesi on 26/02/26.
+//
+
+import SwiftUI
+
+@main
+struct UccelliniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
